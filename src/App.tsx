95505ea088a173bc211715266ca428974412583f
O G Cubillos <ogc3@student.le.ac.uk>
2025-06-15 20:02:07 +0100

    formatting and linting

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import './App.css';
 import content from './content.json';
 
 function App() {
-
   return (
     <>
-      <div>
-      </div>
+      <div></div>
       <h1>{content.landingPage.title}</h1>
       <div className="paragraphs-container">
         <p>{content.landingPage.paragraphs.greeting}</p>
@@ -14,10 +12,10 @@ function App() {
         <p>{content.landingPage.paragraphs.passions}</p>
         <p>{content.landingPage.paragraphs.philosophy}</p>
       </div>
-      
-      <footer className='footer'>
+
+      <footer className="footer">
         <p>Relevant links, if you're interested</p>
-        <div className='footer-links'>
+        <div className="footer-links">
           <a href={content.landingPage.links.instagram}>Instagram</a>
           <a href={content.landingPage.links.pinterest}>Pinterest</a>
           <a href={content.landingPage.links.youtube}>Youtube</a>
